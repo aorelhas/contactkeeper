5ad8ee4146ec3591b7a102be5d6dc78776d5b75b
Carlos Orelhas <c.orelhas@gmail.com>
2020-03-23 21:59:25 +0000

    Errors implemented

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import AlertContext from "../../context/alert/alertContext";
 
 export const Register = () => {
+  const alertContext = useContext(AlertContext);
+
+  const { setAlert } = alertContext;
+
   const [user, setUser] = useState({
     name: "",
     email: "",
@@ -11,9 +16,14 @@ export const Register = () => {
   const { name, email, password, password2 } = user;
 
   const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
+
   const onSubmit = e => {
     e.preventDefault();
-    console.log("Regiter submit");
+    if (name === "" || email === "" || password === "") {
+      setAlert("Please Enter all fields", "danger");
+    } else if (password !== password2) {
+      setAlert("Password do not match", "danger");
+    } else console.log("Regiter submit");
   };
 
   return (
@@ -24,11 +34,23 @@ export const Register = () => {
       <form onSubmit={onSubmit}>
         <div className="form-group">
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" value={name} onChange={onChange} />
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={onChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="email">Email Address</label>
-          <input type="email" name="email" value={email} onChange={onChange} />
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={onChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="password">Enter password</label>
@@ -37,6 +59,8 @@ export const Register = () => {
             name="password"
             value={password}
             onChange={onChange}
+            required
+            minLength="6"
           />
         </div>
         <div className="form-group">
@@ -46,6 +70,8 @@ export const Register = () => {
             name="password2"
             value={password2}
             onChange={onChange}
+            required
+            minLength="6"
           />
         </div>
         <input
